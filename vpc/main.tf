ca4447cd9693d949316bd3ff27e8b53b419cc102
gandhijainam7 <gandhijainam7@gmail.com>
2026-08-26 00:25:26 +0530

    Update main.tf
    2nd  VPC Added

--- vpc/main.tf
+++ vpc/main.tf
@@ -32,3 +32,22 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  public_subnets  = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
