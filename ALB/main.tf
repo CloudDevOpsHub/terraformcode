edd99ee8afab33e51ae154fd716c4da5bf16c18c
shashwat-kotiyal <kotiyalshashwat@gmail.com>
2026-08-25 22:48:13 +0530

    ch

--- ALB/main.tf
+++ ALB/main.tf
@@ -60,6 +60,7 @@ resource "aws_route_table_association" "public_1" {
   route_table_id = aws_route_table.public.id
 }
 
+
 resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public.id
