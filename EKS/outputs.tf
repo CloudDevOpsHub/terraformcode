19a005b345aef3344aa7bfee229f131e78348b3d
Himanshu Shekhar <43611225+Shekh1995@users.noreply.github.com>
2026-08-25 03:35:41 -0700

    Delete EKS directory

--- EKS/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cluster_name" { value = module.eks.cluster_name }
-output "cluster_endpoint" { value = module.eks.cluster_endpoint }
-output "cluster_arn" { value = module.eks.cluster_arn }
-output "vpc_id" { value = module.vpc.vpc_id }
