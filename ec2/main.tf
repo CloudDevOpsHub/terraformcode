e8181236fffd10ba94baddc6b5c6e83c748d18c7
ankit-r-r <ankitrupchandani@gmail.com>
2026-08-25 08:32:45 +0530

    Modify AWS provider and add EC2 instance resource
    Updated AWS provider region and created a new EC2 instance resource with output for public IPs.

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,24 +1,17 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
-  profile = "default"
-  region  = "us-east-2"
+  region = "ap-south-1" # Change to your preferred region
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-064ff912f78e3e561"
-  instance_type = "t3.micro"
+resource "aws_instance" "terraform_server" {
+  ami                    = "ami-0f559c3642608c138"
+  instance_type          = "t3.micro"
+  count                  = 1
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "tf_server"
   }
+}
+
+output "ec2_public_ips" {
+  value = aws_instance.terraform_server[*].public_ip
 }
