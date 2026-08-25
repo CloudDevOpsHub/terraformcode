3c2621ae4c19b745e0f8a0616bf894eb94978f69
Himanshu Shekhar <43611225+Shekh1995@users.noreply.github.com>
2026-08-25 03:30:05 -0700

    Update variables.tf

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -1,3 +1,17 @@
-variable "region" { default = "us-east-2" }
-variable "cluster_name" { default = "terraform-eks-demo" }
-variable "kubernetes_version" { default = "1.36" }
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "terraform-eks-demo"
+}
+
+variable "kubernetes_version" {
+  description = "EKS Kubernetes version"
+  type        = string
+  default     = "1.36"
+}
